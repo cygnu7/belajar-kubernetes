0aad693229a85737a5acf556030c9c0595efdb70
cygnu7 <cygnus7seven@gmail.com>
2022-07-23 23:56:58 +0700

    update reason why use kubernetes

---
+++ kubernetes-theory.sh
@@ -0,0 +1,6 @@
+# Why Use Kubernetes
+- Change from Monolith to Microservices
+- Easy to Scaling Apps
+- Change from Virtual Machine to Container
+- Only Need 1 OS for all Container
+- Kubernetes support many Container Manager
