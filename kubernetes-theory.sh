26b171c4648fdfaff0ca101c0875e90a9d36bbd1
cygnu7 <cygnus7seven@gmail.com>
2022-07-24 00:03:29 +0700

    update kubernetes history

--- kubernetes-theory.sh
+++ kubernetes-theory.sh
@@ -4,3 +4,11 @@
 - Change from Virtual Machine to Container
 - Only Need 1 OS for all Container
 - Kubernetes support many Container Manager
+
+# What is Kubernetes
+Kubernetes is Apps to do Automation Deployment, Scaling, and Container Manager
+
+# Kubernetes History
+- At first, Google Invented Internal System called Borg (then the name changed to Omega).
+- This system help Google to manage many Server.
+- Google Published Kubernetes at 2014, as an Open Source System, based on Omega Experience.
